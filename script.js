7d0db098c4946f38c6727bc79f25fae45b680488
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2019-09-02 00:44:38 +0300

    02/09 0.44

--- script.js
+++ script.js
@@ -8,83 +8,32 @@ $(document).ready(function(){
     $('.menu-close').removeClass('change');
   });
 
-  if (window.innerWidth>=900){
+  $('.close').click(function(){
+    $('.popup').fadeOut();
+  });
+
+  $('.nextSlide').click(function(){
+    $(this).parent('.calcSlide').removeClass('active').next('.calcSlide').addClass('active');
+  });
+
+  if (window.innerWidth>900 || window.innerHeight>600){
     $('#fullpage').fullpage({
       //options here
       autoScrolling:true,
       scrollHorizontally: true
     });
 
-    
- /* $('#fullpage').fullpage({
-    //options here
-    scrollingSpeed: 700,
-    responsiveWidth:800,
-    easingcss3: 'ease-in',
-    scrollBar: false,
-    scrollOverflow: false,
-    licenceKey:'',
-    parallax: true,
-    parallaxKey: '',
-    parallaxOptions: {
-        type: 'reveal',
-        percentage: 100
-    },
-    fitToSection:false,
-    lockAnchors: false,
-    navigation: false,
-    navigationPosition: 'right',
-    showActiveTooltip: false,
-    slidesNavigation: false,
-    controlArrows: false,
-    slidesNavPosition: 'bottom',
-    verticalCentered: false,
-    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9'],
-
-    afterLoad: function(anchorLink, index){
-        var loadedSection = $(this);
-
-        //using index
-        if(index == 2 || index == 4 || index == 5 || index == 7 || index == 9){
-          $('.header').addClass('black');
-          $('.scrollDown').addClass('black');
-        }
-    },
-
-    onLeave: function(index, nextIndex, direction){
-        var leavingSection = $(this);
-
-        //after leaving section 2
-        if(index == 2 && direction =='down' || index == 5 && direction =='down'
-          || index == 7 && direction =='down'){
-          $('.header').removeClass('black');
-          $('.scrollDown').removeClass('black');
-        }
-
-        else if(index == 2 && direction == 'up' || index == 4 && direction == 'up' ||
-          index == 7 && direction == 'up' || index == 9 && direction == 'up'){
-          $('.header').removeClass('black');
-          $('.scrollDown').removeClass('black');
-        }
-    }        
-  });*/
-
-  $('.scrollDown').click(function () {
-      $.fn.fullpage.moveSectionDown();
-  });
-  $('#sec9 .scrollDown').click(function () {
-      $.fn.fullpage.moveSectionUp();
-  });
+    $('.scrollDown').click(function () {
+        $.fn.fullpage.moveSectionDown();
+    });
+    $('#sec9 .scrollDown').click(function () {
+        $.fn.fullpage.moveSectionUp();
+    });
 
   }
 
-  
-  else {
-      
-    $(".menu-desktop a").click(function() {
-      $(".menu-desktop").removeClass('active');
-      $('.menu-close').removeClass('change');
-    });
+  if (window.innerWidth<=900 || window.innerHeight<=600) {
+    fullpage_api.setResponsive(true);
 
     $(".menu-desktop a[href='#section2']").attr('href', '#sec2');
     $(".menu-desktop a[href='#section3']").attr('href', '#sec3');
@@ -93,18 +42,26 @@ $(document).ready(function(){
     $(".menu-desktop a[href='#section6']").attr('href', '#sec6');
     $(".menu-desktop a[href='#section7']").attr('href', '#sec7');
     $(".menu-desktop a[href='#section9']").attr('href', '#sec9');
+  }
 
+
+  
+
+  else {
+      
+    $(".menu-desktop a").click(function() {
+      $(".menu-desktop").removeClass('active');
+      $('.menu-close').removeClass('change');
+    });
   }
 
   
 
-  $('.nextSlide').click(function(){
-    $(this).parent('.calcSlide').removeClass('active').next('.calcSlide').addClass('active');
-  });
+  
 
 
   $(window).resize(function(){
-    if (window.innerWidth>=900){
+    if (window.innerWidth>900 || window.innerHeight>600){
       $(".menu-desktop a[href='#sec2']").attr('href', '#section2');
       $(".menu-desktop a[href='#sec3']").attr('href', '#section3');
       $(".menu-desktop a[href='#sec4']").attr('href', '#section4');
@@ -112,8 +69,22 @@ $(document).ready(function(){
       $(".menu-desktop a[href='#sec6']").attr('href', '#section6');
       $(".menu-desktop a[href='#sec7']").attr('href', '#section7');
       $(".menu-desktop a[href='#sec9']").attr('href', '#section9');
-      }
-    else  {
+      
+      fullpage_api.setResponsive(false);
+      fullpage_api.reBuild('all');
+      
+
+      $('.scrollDown').click(function () {
+          $.fn.fullpage.moveSectionDown();
+      });
+      $('#sec9 .scrollDown').click(function () {
+          $.fn.fullpage.moveSectionUp();
+      });
+    }
+
+    if (window.innerWidth<=900 || window.innerHeight<=600) {
+      fullpage_api.setResponsive(true);
+      /*fullpage_api.destroy('all');*/
       $(".menu-desktop a[href='#section2']").attr('href', '#sec2');
       $(".menu-desktop a[href='#section3']").attr('href', '#sec3');
       $(".menu-desktop a[href='#section4']").attr('href', '#sec4');
@@ -127,6 +98,10 @@ $(document).ready(function(){
         $('.menu-close').removeClass('change');
       });
     }
+
+    else  {
+      
+    }
   });
 
   $('#sideSizeTrigger').click(function(){
@@ -136,5 +111,8 @@ $(document).ready(function(){
     $('#sideSizePopup').toggleClass('active');
   });
 
+
+
 });
+
 
